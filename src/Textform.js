814e2e7dc39400ed30fbb6ab63da634352701028
Omkar-Nilakhe <nilakheomkar@gmail.com>
2024-12-27 01:55:14 +0530

    add space1

--- src/Textform.js
+++ src/Textform.js
@@ -71,7 +71,8 @@ export default function Textform(props) {
                 <button disabled={Text.length===0} className="btn btn-primary mx-2 my-1" onClick={SpeakClick} type="submit">Speak Text</button>
                 <button disabled={Text.length===0} className="btn btn-primary mx-2 my-1" onClick={CapitalizedCase} type="submit">Capitalized Text</button>
                 <button disabled={Text.length===0} className="btn btn-primary mx-2 my-1" onClick={removeExtraspaces} type="submit">Remove space</button>
-
+                <button disabled={Text.length===0} className="btn btn-primary mx-2 my-1"  type="submit">Remove space1</button>
+                
             </div>
 
             <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
